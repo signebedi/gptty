f9b9bc3d6150b41d8ca97cb5bacb3acb0e793fcb
signebedi <signe@siftingwinnowing.com>
2023-03-19 14:51:10 -0500

    Added: prelim working interface

--- tests/server.py
+++ tests/server.py
@@ -1,16 +1,44 @@
 import http.server
 import socketserver
-json
+import json
+import asyncio
+import aiohttp
+import tracemalloc
+
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
+    # def do_GET(self):
+    #     response_body = "1"
+    #     response = {"response": response_body}
+    #     response_json = json.dumps(response)
+
+    #     self.send_response(200)
+    #     self.send_header('Content-type', 'application/json')
+    #     self.end_headers()
+    #     self.wfile.write(response_json.encode())
+
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        message = post_data.decode('utf-8')
+
+        response_body = "1"
+        response = {"response": response_body}
+        response_json = json.dumps(response)
+
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(b'1')
+        self.wfile.write(response_json.encode())
+
 
 PORT = 8080
+HOST = '0.0.0.0'
+async def main(loop):
+    server = socketserver.TCPServer((HOST, PORT), MyHandler)
+    print(f"Server listening on {HOST}:{PORT}")
+    await loop.run_in_executor(None, server.serve_forever)
 
-with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
-    print("Server listening on port", PORT)
-    httpd.serve_forever()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main(loop))

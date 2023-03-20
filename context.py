2ecdf5c195747c16439de07a51409d766e5b7849
signebedi <signe@siftingwinnowing.com>
2023-03-19 21:50:30 -0500

    Added: context sharing functions (#7)

---
+++ context.py
@@ -0,0 +1,20 @@
+__name__ = "gptty.context"
+__author__ = "Sig Janoska-Bedi"
+__credits__ = ["Sig Janoska-Bedi"]
+__version__ = "0.1.0"
+__license__ = "MIT"
+__maintainer__ = "Sig Janoska-Bedi"
+__email__ = "signe@atreeus.com"
+
+def get_context(tag, max_context_length, output_file):
+    context = ""
+    with open (output_file,'r') as f:
+        text = f.readlines()
+    
+    for row in text:
+        data = row.replace('\n','').split('|')
+
+        if data[1] == tag:
+            context += data[3]
+    
+    return context

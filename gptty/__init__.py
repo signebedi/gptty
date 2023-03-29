ec4a4483b36d10f8802c6cae3bfe22cdabca4c14
signebedi <signe@siftingwinnowing.com>
2023-03-29 10:46:54 -0500

    Added: __init__.py file (#38)

---
+++ gptty/__init__.py
@@ -0,0 +1,14 @@
+__name__ = "gptty"
+__author__ = "Sig Janoska-Bedi"
+__credits__ = ["Sig Janoska-Bedi"]
+__version__ = "0.2.3"
+__license__ = "MIT"
+__maintainer__ = "Sig Janoska-Bedi"
+__email__ = "signe@atreeus.com"
+
+from gptty import (
+    config, 
+    context, 
+    gptty, 
+    tagging
+)

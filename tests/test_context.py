30bebf130260437d6594bd9b154ad416a00d8ec5
signebedi <signe@siftingwinnowing.com>
2023-03-30 16:10:57 -0500

    Fixed: unittest error from debug mode (#28)

--- tests/test_context.py
+++ tests/test_context.py
@@ -9,12 +9,12 @@ class TestContext(unittest.TestCase):
         self.assertEqual(result, ['lazy dog', 'quick brown fox jumps'])
 
     def test_get_context_keywords_only(self):
-        result = get_context("Tag1", 50, "tests/test_context_data.txt", context_keywords_only=True, question="Who is its mayor?")
+        result = get_context("Tag1", 50, "tests/test_context_data.txt", "text-davinci-003", context_keywords_only=True, question="Who is its mayor?")
         expected = "australia canberra 's capital city Who is its mayor?"
         self.assertEqual(result.strip(), expected.strip())
 
     def test_get_context_no_keywords(self):
-        result = get_context("Tag1", 50, "tests/test_context_data.txt", context_keywords_only=False, question="Who is its mayor?")
+        result = get_context("Tag1", 50, "tests/test_context_data.txt", "text-davinci-003", context_keywords_only=False, question="Who is its mayor?")
         expected = "what is the capital of australia? The capital of Australia is Canberra. when was it founded? Canberra was founded in 1913 as the site for Australia's capital city. Who is its mayor?"
         self.assertEqual(result.strip(), expected.strip())
 
